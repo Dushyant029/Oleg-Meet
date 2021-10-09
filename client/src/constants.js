8fc579936fded15f1afedfc8c369e0eca9ac4f8b
K. Dushyant Reddy <dushireddy291@gmail.com>
2021-10-09 21:55:22 +0530

    url chng

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 export const APP_NAME = 'Oleg';
 export const repoName = 'https://github.com/dushyant029';
-export const APP_URL = 'https://oleg.netlify.app';
+export const APP_URL = 'https://olegmeet.netlify.app';
 export const BACKEND_URL = 'https://oleg-meet-backend.herokuapp.com';
